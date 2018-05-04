6025c049de9eaa076d96ef1dfb4f7f61fe79a77a
jfleck91 <jfleck91@gmail.com>
2018-05-04 11:47:00 -0700

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('turkey burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', true);
