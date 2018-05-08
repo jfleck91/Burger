558f788888189400ebead780f7a47a6e4409e8c2
jfleck91 <jfleck91@gmail.com>
2018-05-07 17:10:56 -0700

    updated

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,26 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+// Set up database connection
+var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "George91",
-  database: "burgers_db"
-});
+// add in the environment variable option for JAWSDB for heroku
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'George91',
+		database: 'burgers_db'
+	});
+};
 
-// Make connection.
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+	if (err) {
+		console.error('error conencting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+// export the connection back to orm
 module.exports = connection;
-
-
